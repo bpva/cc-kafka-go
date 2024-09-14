bc9850baa194a3e6add919dd8a38cee5694156ed
bpva <bpva@protonmail.com>
2024-09-14 19:44:30 +0300

    add correlation id echoing

--- app/server.go
+++ app/server.go
@@ -1,13 +1,31 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
+	"io"
 	"net"
 	"os"
 )
 
 var _ = net.Listen
 var _ = os.Exit
+
+type responseHeader struct {
+	CorrelationId int32
+}
+
+type response struct {
+	len int32
+	hdr responseHeader
+}
+
+func (r *response) bytes() []byte {
+	buf := make([]byte, 8)
+	binary.BigEndian.PutUint32(buf, uint32(r.len))
+	binary.BigEndian.PutUint32(buf[4:], uint32(r.hdr.CorrelationId))
+	return buf
+}
 
 func main() {
 	fmt.Println("Logs from your program will appear here!")
@@ -27,15 +45,35 @@ func main() {
 
 	fmt.Println("Connection accepted")
 
-	buff := make([]byte, 1024)
+	lengthBuf := make([]byte, 4)
 
-	_, err = conn.Read(buff)
+	_, err = io.ReadFull(conn, lengthBuf)
 	if err != nil {
-		fmt.Println("Error reading:", err.Error())
+		fmt.Println("Error reading length:", err.Error())
 		os.Exit(1)
 	}
 
-	_, err = conn.Write([]byte{0, 0, 0, 0, 0, 0, 0, 7})
+	length := binary.BigEndian.Uint32(lengthBuf)
+	fmt.Println("Length:", length)
+
+	messageBuf := make([]byte, length)
+	_, err = io.ReadFull(conn, messageBuf)
+	if err != nil {
+		fmt.Println("Error reading message:", err.Error())
+		os.Exit(1)
+	}
+
+	correlationId := binary.BigEndian.Uint32(messageBuf[4:8])
+
+	resp := response{
+		len: 0,
+		hdr: responseHeader{
+			CorrelationId: int32(correlationId),
+		},
+	}
+
+	_, err = conn.Write(resp.bytes())
+
 	if err != nil {
 		fmt.Println("Error writing:", err.Error())
 		os.Exit(1)
