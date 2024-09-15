1a01c616557d96075741691788bf9b8805079ded
bpva <bpva@protonmail.com>
2024-09-15 08:18:15 +0300

    add concurrency

--- app/server.go
+++ app/server.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"os"
-	"time"
 )
 
 func main() {
@@ -16,37 +16,49 @@ func main() {
 		os.Exit(1)
 	}
 
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
-	conn.SetReadDeadline(time.Now().Add(10 * time.Second))
-	defer conn.Close()
-
-	fmt.Println("Connection accepted")
-
 	for {
-		handleConnection(conn)
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Failed to accept connection")
+			os.Exit(1)
+		}
+
+		go handleConnection(conn)
 	}
 }
 
 func handleConnection(conn net.Conn) {
-	buf := make([]byte, 1024)
-	br, _ := conn.Read(buf)
+	var eof bool
+	defer func() {
+		fmt.Println("Closing connection")
+		conn.Close()
+	}()
+	for !eof {
+		buf := make([]byte, 1024)
+		br, err := conn.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				fmt.Println("EOF")
+				eof = true
+				break
+			}
+			fmt.Println("Error reading:", err.Error())
+			return
+		}
 
-	req := requestFromBytes(buf)
-	if req.hdr.len != int32(br-4) {
-		fmt.Printf("header length %d does not match buffer length %d\n", req.hdr.len, br-4)
-		errResp := makeErrorResponce(req.hdr.correlationId, CORRUPT_MESSAGE)
-		conn.Write(errResp.bytes())
-		return
-	}
-	resp := makeResponse(req)
+		req := requestFromBytes(buf)
+		if req.hdr.len != int32(br-4) {
+			fmt.Printf("header length %d does not match buffer length %d\n", req.hdr.len, br-4)
+			errResp := makeErrorResponce(req.hdr.correlationId, CORRUPT_MESSAGE)
+			conn.Write(errResp.bytes())
+			return
+		}
+		resp := makeResponse(req)
 
-	fmt.Printf("Responding with: %v\n", resp.bytes())
-	_, err := conn.Write(resp.bytes())
-	if err != nil {
-		fmt.Println("Error writing:", err.Error())
+		fmt.Printf("Responding with: %v\n", resp.bytes())
+		_, err = conn.Write(resp.bytes())
+		if err != nil {
+			fmt.Println("Error writing:", err.Error())
+		}
 	}
 }
